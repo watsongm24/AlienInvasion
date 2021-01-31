b5f89c34e9e2c58897246892193b2544796e824b
watsongm24 <watsonmarcelain@gmail.com>
2021-01-31 01:04:08 -0400

    Drawing the ship to the Screen

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    # A class to store all the setings for Allien Invasion.
+    
+    def __init__(self):
+        #Initialize the game's Settings. 
+        #Screen Settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
